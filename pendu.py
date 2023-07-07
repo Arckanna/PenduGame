41250e80a677fe17735c57bedf94ceed2887efae
valerie lecoeur <valerie.lecoeur75@gmail.com>
2023-07-07 18:01:35 +0200

    mise en place du debut de code

---
+++ pendu.py
@@ -0,0 +1,7 @@
+import random
+
+words = "sangle  Zombi Croix Rouge Catastrophe Gym Chatouiller Lucifer Ardent Librairie"
+
+while True:
+    lettre = input('Entrer une lettre : ')
+    print(lettre)
